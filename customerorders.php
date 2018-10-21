b50a5e988a30a8c94062a4dafe660fafc7dfd93c
Maciej Adamczewski <zapalniczek@gmail.com>
2018-10-22 00:34:59 +0200

    optional version

--- customerorders.php
+++ customerorders.php
@@ -26,8 +26,16 @@ $ret['$i']['orderdate']=$order->created_at;
 $ret['$i']['ordertotal']=$order->grand_total;
 //$ret[$i]['orderid']=$order->order_id;
 
-
-
+    //optional
+//$objectManager = \Magento\Framework\App\ObjectManager::getInstance();
+//$lastyear = date('Y-m-d', strtotime("-1 year"));
+//$orderCollection = $objectManager->create('\Magento\Sales\Model\ResourceModel\Order\Collection');
+//$orderCollection->addAttributeToFilter('customer_id',123456)
+//	        ->addAttributeToFilter('status','complete')
+//	        ->addAttributeToFilter('created_at', array('gteq'  => $lastyear))->load();
+//
+//echo "<pre>";print_r($orderCollection->getData()); exit;	
+// end optional
 
 
 endforeach;
